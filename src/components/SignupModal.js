2176757efbb8434be531fcefec06760d504e65ed
Amanda So <soamanda95@gmail.com>
2023-01-18 23:01:13 -0800

    send http request to sign up user

--- src/components/SignupModal.js
+++ src/components/SignupModal.js
@@ -2,11 +2,58 @@ import React, { useState } from "react";
 
 const SignupModal = () => {
   const [isActive, setIsActive] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState(null);
   const toggleModal = () => {
     if (isActive) {
       setIsActive(false);
     } else {
       setIsActive(true);
+    }
+  };
+
+  const clickSignup = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:3000/blog/sign-up", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          password_confirmation: confirmation,
+          author: false,
+        }),
+      });
+      let resJson = await res.json();
+
+      if (res.status === 200) {
+        if (resJson.error) {
+          setError(resJson.error);
+        }
+        if (resJson.errors) {
+          setErrors(resJson.errors);
+        }
+        if (resJson.message) {
+          setUsername("");
+          setPassword("");
+          setConfirmation("");
+          setError(null);
+          setErrors(null);
+          toggleModal();
+          alert(resJson.message);
+        }
+      } else {
+        console.log("error occurred");
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -29,38 +76,65 @@ const SignupModal = () => {
             </header>
             <section className="modal-card-body">
               <div className="content">
-                <div className="field">
-                  <label className="label">Username</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="e.g johnsmith"
-                    />
+                <form>
+                  <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="text"
+                        placeholder="e.g johnsmith"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field">
-                  <div className="label">Password</div>
-                  <div className="control">
-                    <input className="input" type="password" />
+                  <div className="field">
+                    <div className="label">Password</div>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field">
-                  <div className="label">Password Confirmation</div>
-                  <div className="control">
-                    <input className="input" type="password" />
+                  <div className="field">
+                    <div className="label">Password Confirmation</div>
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="password"
+                        value={confirmation}
+                        onChange={(e) => setConfirmation(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="field is-grouped">
-                  <p className="control">
-                    <button className="button">Submit</button>
-                  </p>
-                  <p className="control">
-                    <button className="button" onClick={() => toggleModal()}>
-                      Cancel
-                    </button>
-                  </p>
-                </div>
+                  <div className="field is-grouped">
+                    <p className="control">
+                      <button
+                        className="button"
+                        onClick={(e) => clickSignup(e)}
+                      >
+                        Submit
+                      </button>
+                    </p>
+                    <p className="control">
+                      <button className="button" onClick={() => toggleModal()}>
+                        Cancel
+                      </button>
+                    </p>
+                  </div>
+                </form>
+                {error ? <p>{error}</p> : null}
+                {errors ? (
+                  <ul>
+                    {errors.map((item, index) => (
+                      <li key={index}>{item.msg}</li>
+                    ))}
+                  </ul>
+                ) : null}
               </div>
             </section>
           </div>
